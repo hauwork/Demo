e9f9b665e37ebedcb61c6effe9d471585e5d02a5
Ab <abettbl@Suriko.slyngelnet.org>
2014-09-19 13:06:17 +0200

    ET NYT COMMIT

---
+++ src/demoprojekt/BobArne.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package demoprojekt;
+
+/**
+ *
+ * @author abettbl
+ */
+public class BobArne {
+    
+    private String = "Bob-Arne";
+    // JEG KAN LAVE PUSH LOL!
+    
+}
